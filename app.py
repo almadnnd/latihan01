c1236bd5e8d2a61a65a39f06727b38414bd6d940
alma <almadnnd@gmail.com>
2025-12-27 21:16:41 +0700

    Menyelesaikan tugas

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 import streamlit as st
 
 pages = [
-    st.Page(page="pages/page1.py", title="Pendahuluan", icon="📑"),
-    st.Page(page="pages/page2.py", title="Hasil dan Pembahasan", icon="📑"),
-    st.Page(page="pages/page3.py", title="Penutup", icon="📑"),
-    st.Page(page="pages/page4.py", title="Visualisasi Data", icon="📑"),
+    st.Page(page="pages/page1.py", title="Pendahuluan"),
+    st.Page(page="pages/page2.py", title="Hasil dan Pembahasan"),
+    st.Page(page="pages/page3.py", title="Penutup"),
+    st.Page(page="pages/page4.py", title="Visualisasi Data"),
 ]
 
 pg = st.navigation(
