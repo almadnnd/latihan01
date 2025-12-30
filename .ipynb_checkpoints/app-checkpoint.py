ce6de06914c1e61d3394f9a8bcda1baa6711365c
alma <almadnnd@gmail.com>
2025-12-30 19:41:49 +0700

    Menyelesaikan tugas.

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+pages = [
+    st.Page(page="pages/page1.py", title="Pendahuluan"),
+    st.Page(page="pages/page2.py", title="Hasil dan Pembahasan"),
+    st.Page(page="pages/page3.py", title="Penutup"),
+    st.Page(page="pages/page4.py", title="Visualisasi Data"),
+]
+
+pg = st.navigation(
+    pages,
+    position="sidebar"
+)
+
+pg.run()
